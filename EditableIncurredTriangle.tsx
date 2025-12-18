6ea9b1b97970ce5266f542cd9b7636324bb01538
kryproszc <62724175+kryproszc@users.noreply.github.com>
2025-12-18 16:08:43 +0100

    Update EditableIncurredTriangle.tsx

--- EditableIncurredTriangle.tsx
+++ EditableIncurredTriangle.tsx
@@ -1,11 +1,1 @@
-Dzień dobry,
-
-przesyłam przygotowaną inicjatywę dotyczącą uruchomienia dedykowanego środowiska wirtualnego, przeznaczonego do uruchamiania i eksploatacji aplikacji wspierających procesy analityczne, raportowe i kontrolne realizowane w Departamencie DIU.
-
-Środowisko to będzie stanowiło wspólną platformę techniczną, umożliwiającą jednoczesne korzystanie z przygotowywanych aplikacji przez wielu użytkowników. W tym celu planowane jest wdrażanie aplikacji w oparciu o technologię konteneryzacji (Docker), co pozwoli na ich standaryzację, łatwiejsze utrzymanie oraz efektywne zarządzanie środowiskiem.
-
-Po uruchomieniu środowiska wirtualnego możliwe będzie wdrożenie przygotowanych aplikacji służących m.in. do ewidencji kontroli oraz oceny i monitorowania rezerw techniczno-ubezpieczeniowych i modeli wewnętrznych, co pozwoli na lepsze zrozumienie decyzji podejmowanych przez zakłady ubezpieczeń w tych obszarach.
-
-Pozostaję do dyspozycji w razie pytań lub potrzeby doprecyzowania zakresu inicjatywy.
-
-Pozdrawiam
+d
