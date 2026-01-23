e5b241721db09b3e8437911a7dcf4cf1f4454bff
kryproszc <62724175+kryproszc@users.noreply.github.com>
2026-01-23 12:00:40 +0100

    Update EditableIncurredTriangle.tsx

--- EditableIncurredTriangle.tsx
+++ EditableIncurredTriangle.tsx
@@ -1,22 +1,5 @@
-install.packages("remotes")
-library(remotes)
-
-
-install_version("vctrs",        "0.6.5")
-install_version("scales",       "1.4.0")
-install_version("treemapify",   "2.5.6")
-install_version("cowplot",      "1.2.0")
-install_version("tinytex",      "0.57")
-install_version("shinythemes",  "1.2.0")
-install_version("plotly",       "4.11.0")
-install_version("flexdashboard","0.9.10")
-install_version("RSQLite",      "2.4.3")
-install_version("tidyverse",    "2.0.0")
-install_version("here",         "1.0.2")
-install_version("usethis",      "3.1.0")
-install_version("devtools",     "2.4.5")
-install_version("rmarkdown",    "2.30")
-install_version("shinyjs",      "2.1.0")
-install_version("shiny",        "1.10.0")
-
-
+install.packages(
+  "https://cran.r-project.org/src/contrib/Archive/shiny/shiny_1.10.0.tar.gz",
+  repos = NULL,
+  type = "source"
+)
