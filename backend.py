c5477f17cade0c3fedb9f061efae22a36b804d9b
kryproszc <62724175+kryproszc@users.noreply.github.com>
2026-02-24 09:59:24 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,6 +1,2 @@
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-tables = cursor.fetchall()
-
-print("\nTabele w bazie:")
-for t in tables:
-    print("-", t[0])
+.\venv\Scripts\Activate.ps1
+venv\Scripts\Activate.ps1
