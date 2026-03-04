f3fda5a67f84d8af94413261aa6a0838de0fb6ce
kryproszc <62724175+kryproszc@users.noreply.github.com>
2026-03-04 16:36:29 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,1 +1,1 @@
-runas /netonly /user:IT\ss_snu_qrt "cmd /k C:\Python311\python.exe test_runas.py"
+runas /netonly /user:IT\ss_snu_qrt "cmd /k cd /d I:\WOM\Modele\Monitorowanie\Narzędzie_do_monitorowania_modeli\Test baza sqlite3 && C:\Python311\python.exe python_download_all_views.py"
