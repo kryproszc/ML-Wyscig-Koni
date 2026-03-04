f464f41a7a3309a0b3fb4c2396b98a5b09d25d2b
kryproszc <62724175+kryproszc@users.noreply.github.com>
2026-03-04 16:21:15 +0100

    Implement view retrieval and storage in SQLite
    Added functionality to retrieve and save SQL Server views to a local SQLite database.

--- backend.py
+++ backend.py
@@ -1,14 +1,43 @@
 import pyodbc
+import pandas as pd
+import sqlite3
 
-conn = pyodbc.connect(
+# połączenie z SQL Server
+conn_sql = pyodbc.connect(
     "DRIVER={ODBC Driver 17 for SQL Server};"
     "SERVER=DTC-NEWM5-SNU;"
     "DATABASE=SNU_ANA;"
     "Trusted_Connection=yes;"
 )
 
-cursor = conn.cursor()
-cursor.execute("SELECT TOP 5 * FROM dbo.V_S_02_01_01_01_DIU")
+cursor = conn_sql.cursor()
 
-for row in cursor.fetchall():
-    print(row)
+# lista widoków
+views_query = """
+SELECT TABLE_NAME
+FROM INFORMATION_SCHEMA.VIEWS
+"""
+
+views = [row[0] for row in cursor.execute(views_query)]
+
+print("Znalezione widoki:", len(views))
+
+# lokalna baza
+conn_local = sqlite3.connect("local_snu_ana.db")
+
+for view in views:
+    print("Pobieram:", view)
+
+    df = pd.read_sql(f"SELECT * FROM {view}", conn_sql)
+
+    df.to_sql(view, conn_local, if_exists="replace", index=False)
+
+
+
+
+
+
+
+runas /netonly /user:IT\ss_snu_qrt "python download_all_views.py"
+
+print("Gotowe! Wszystkie widoki zapisane do local_snu_ana.db")
