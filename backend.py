8cbb063a0b31e09c87f23e86b40a2fb9c9d0fcb2
kryproszc <62724175+kryproszc@users.noreply.github.com>
2026-03-04 16:16:13 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,1 +1,14 @@
-runas /netonly /user:IT\ss_snu_qrt "python test_na_brudno.py"
+import pyodbc
+
+conn = pyodbc.connect(
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=DTC-NEWM5-SNU;"
+    "DATABASE=SNU_ANA;"
+    "Trusted_Connection=yes;"
+)
+
+cursor = conn.cursor()
+cursor.execute("SELECT TOP 5 * FROM dbo.V_S_02_01_01_01_DIU")
+
+for row in cursor.fetchall():
+    print(row)
