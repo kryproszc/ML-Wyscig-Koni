e29c5caa5c5f11f4e390bfaf3f924caea2e3d104
kryproszc <62724175+kryproszc@users.noreply.github.com>
2026-03-04 11:25:14 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,2 +1,21 @@
-.\venv\Scripts\Activate.ps1
-venv\Scripts\Activate.ps1
+import pyodbc
+import pandas as pd
+
+server = "ADRES_SERWERA"
+database = "NAZWA_BAZY"
+username = "LOGIN"
+password = "HASLO"
+
+conn = pyodbc.connect(
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    f"SERVER={server};"
+    f"DATABASE={database};"
+    f"UID={username};"
+    f"PWD={password}"
+)
+
+query = "SELECT * FROM dbo.NAZWA_WIDOKU"
+
+df = pd.read_sql(query, conn)
+
+print(df.head())
