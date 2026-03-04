bcd30c8935ce0ee3d08e8bad6cf67043c4e3a410
kryproszc <62724175+kryproszc@users.noreply.github.com>
2026-03-04 15:36:51 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,35 +2,12 @@ import pyodbc
 
 server = "DTC-NEWM5-SNU"
 database = "SNU_ANA"
-username = "ss_snu_qrt"
-password = "TWOJE_HASLO"
-
-conn_str = (
-    "DRIVER={ODBC Driver 17 for SQL Server};"
-    f"SERVER={server};"
-    f"DATABASE={database};"
-    f"UID={username};"
-    f"PWD={password};"
-)
-
-try:
-    conn = pyodbc.connect(conn_str)
-    print("Połączenie OK")
-except Exception as e:
-    print("Błąd:", e)
-
-
-
------
-
-
-import pyodbc
 
 conn = pyodbc.connect(
     "DRIVER={ODBC Driver 17 for SQL Server};"
-    "SERVER=DTC-NEWM5-SNU;"
-    "DATABASE=SNU_ANA;"
+    f"SERVER={server};"
+    f"DATABASE={database};"
     "Trusted_Connection=yes;"
 )
 
-print("Połączono")
+print("Połączono z bazą!")
