7977e3ff2e27847a438cf789ad9ea48ceddbff6f
kryproszc <62724175+kryproszc@users.noreply.github.com>
2026-03-05 09:35:11 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,18 +1,10 @@
 import sqlite3
-from pathlib import Path
+import pandas as pd
 
 conn = sqlite3.connect(r"I:\WOM\Modele\Monitorowanie\Narzędzie_do_monitorowania_modeli\Wskazniki_Baza\Baza\local_snu_ana.db")
-cursor = conn.cursor()
 
-sql_file = Path(r"I:\WOM\Modele\Monitorowanie\Narzędzie_do_monitorowania_modeli\Wskazniki_Baza\Kod_SQL\Ekspozycja na ryzyko\calkowity_wymog.sql")
+df = pd.read_sql_query("SELECT * FROM calkowity_wymog", conn)
 
-with open(sql_file, "r", encoding="utf-8") as f:
-    sql = f.read()
+print(df)
 
-# usuwamy schemat SQL Server tylko dla SQLite
-sql = sql.replace("SNU_ANA.dbo.", "")
-
-cursor.executescript(sql)
-
-conn.commit()
 conn.close()
