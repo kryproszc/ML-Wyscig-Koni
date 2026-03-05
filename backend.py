f4b240cc93f503b1860b3d969218d51b26efecf0
kryproszc <62724175+kryproszc@users.noreply.github.com>
2026-03-05 09:33:03 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,13 +1,16 @@
 import sqlite3
 from pathlib import Path
 
-conn = sqlite3.connect(r"I:\WOM\Modele\Monitoro\Baza\local_snu_ana.db")
+conn = sqlite3.connect(r"I:\WOM\Modele\Monitorowanie\Narzędzie_do_monitorowania_modeli\Wskazniki_Baza\Baza\local_snu_ana.db")
 cursor = conn.cursor()
 
-sql_file = Path(r"I:\WOM\Modele\Monitoro\Kod_SQL\Ekspozycja na ryzyko\calkowity_wymog.sql")
+sql_file = Path(r"I:\WOM\Modele\Monitorowanie\Narzędzie_do_monitorowania_modeli\Wskazniki_Baza\Kod_SQL\Ekspozycja na ryzyko\calkowity_wymog.sql")
 
 with open(sql_file, "r", encoding="utf-8") as f:
     sql = f.read()
+
+# usuwamy schemat SQL Server tylko dla SQLite
+sql = sql.replace("SNU_ANA.dbo.", "")
 
 cursor.executescript(sql)
 
