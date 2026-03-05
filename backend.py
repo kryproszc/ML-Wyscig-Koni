f3ceb42262ebf56947ae15ae94256234a7a7411d
kryproszc <62724175+kryproszc@users.noreply.github.com>
2026-03-05 18:44:18 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,17 +1,13 @@
-CASE
-    WHEN v17a.C0180 IS NOT NULL THEN
-        (v25.C0010 / NULLIF(v05a.C0200,0)) *
-        CASE 
-            WHEN v25.TYP_OKRESU = 'R' THEN 1
-            ELSE MONTH(v25.DATA_SPR) / 12.0
-        END
-
-    WHEN v17b.C0180 IS NOT NULL THEN
-        (v25.C0010 / NULLIF(v05b.C0200,0)) *
-        CASE 
-            WHEN v25.TYP_OKRESU = 'R' THEN 1
-            ELSE MONTH(v25.DATA_SPR) / 12.0
-        END
-
-    ELSE NULL
-END AS SCR_majatkowy_do_skladka_zarobiona
+WITH suma_R0960 AS
+(
+    SELECT
+        DATA_SPR,
+        TYP_OKRESU,
+        KOD_ZU,
+        SUM(R0960) AS SUMA_R0960
+    FROM SNU_ANA.dbo.V_S_26_13_01_07_DIU
+    GROUP BY
+        DATA_SPR,
+        TYP_OKRESU,
+        KOD_ZU
+)
