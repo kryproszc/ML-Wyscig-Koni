275dd72423f9cf6cbea011a7dafc89f77b51062a
kryproszc <62724175+kryproszc@users.noreply.github.com>
2026-03-05 12:27:36 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,10 +1,27 @@
-v25.C0010 / NULLIF(v05a.C0200 *
-(
 CASE
-    WHEN v25.TYP_OKRESU = 'R' THEN 4
-    WHEN v25.TYP_OKRESU = 'K' AND MONTH(v25.DATA_SPR)=3 THEN 1
-    WHEN v25.TYP_OKRESU = 'K' AND MONTH(v25.DATA_SPR)=6 THEN 2
-    WHEN v25.TYP_OKRESU = 'K' AND MONTH(v25.DATA_SPR)=9 THEN 3
-    WHEN v25.TYP_OKRESU = 'K' AND MONTH(v25.DATA_SPR)=12 THEN 4
-END
-)/4,0)
+    WHEN v17a.C0180 IS NOT NULL
+        THEN v25.C0010 /
+             NULLIF(
+                 v05a.C0200 *
+                 (
+                     CASE 
+                         WHEN v25.TYP_OKRESU = 'R' THEN 4
+                         ELSE MONTH(v25.DATA_SPR) / 3
+                     END
+                 ) / 4
+             ,0)
+
+    WHEN v17b.C0180 IS NOT NULL
+        THEN v25.C0010 /
+             NULLIF(
+                 v05b.C0200 *
+                 (
+                     CASE 
+                         WHEN v25.TYP_OKRESU = 'R' THEN 4
+                         ELSE MONTH(v25.DATA_SPR) / 3
+                     END
+                 ) / 4
+             ,0)
+
+    ELSE NULL
+END AS SCR_majatkowy_do_skladka_zarobiona
